9fd94f40566dac235438f524cbb9d95427a584b0
mathrose <mrosa@untref.edu.ar>
2023-10-01 20:02:43 -0300

    Color de textos mal saturados corregido.
    Se corrigieron los colores de los textos que saturaban de manera incorrecta con un fondo azul.

--- combate-criaturas/UIAtaques.java
+++ combate-criaturas/UIAtaques.java
@@ -4,93 +4,93 @@ import greenfoot.GreenfootImage;
 import greenfoot.World;
 
 public class UIAtaques extends Actor {
-	Criatura criaturaActual;
-	Texto tituloAtaques;
-	Boton b1, b2, b3, b4;
-	Criatura[] criaturas;
-	Boton botonSeleccionado;
-	Parrafo descripcion;
-	Runnable ataque = null;
-	Criatura ataqueObjetivo = null;
-	Criatura hoverObjetivo = null;
+    Criatura criaturaActual;
+    Texto tituloAtaques;
+    Boton b1, b2, b3, b4;
+    Criatura[] criaturas;
+    Boton botonSeleccionado;
+    Parrafo descripcion;
+    Runnable ataque = null;
+    Criatura ataqueObjetivo = null;
+    Criatura hoverObjetivo = null;
 
-	public UIAtaques(Criatura[] criaturas) {
-		this.criaturas = criaturas;
+    public UIAtaques(Criatura[] criaturas) {
+        this.criaturas = criaturas;
 
-		b1 = new Boton("", null, 25, Color.BLACK, 320, 30);
-		b2 = new Boton("", null, 25, Color.BLACK, 320, 30);
-		b3 = new Boton("", null, 25, Color.BLACK, 320, 30);
-		b4 = new Boton("", null, 30, Color.BLACK, 320, 36);
-		descripcion = new Parrafo("Ataque", 20, Color.BLACK, 344, 192);
-		tituloAtaques = new Texto("Ataques", 30, Color.BLACK, null);
-	}
+        b1 = new Boton("", null, 25, Color.BLACK, 320, 30);
+        b2 = new Boton("", null, 25, Color.BLACK, 320, 30);
+        b3 = new Boton("", null, 25, Color.BLACK, 320, 30);
+        b4 = new Boton("", null, 30, Color.BLACK, 320, 36);
+        descripcion = new Parrafo("Ataque", 20, Color.BLACK, 344, 192);
+        tituloAtaques = new Texto("Ataques", 30, Color.WHITE, null);
+    }
 
-	@Override
-	protected void addedToWorld(World world) {
-		getWorld().addObject(tituloAtaques, 170, 225);
-		getWorld().addObject(b1, 176, 265);
-		getWorld().addObject(b2, 176, 300);
-		getWorld().addObject(b3, 176, 335);
-		getWorld().addObject(b4, 176, 373);
-		getWorld().addObject(descripcion, 352 + 344 / 2, 300);
-	}
+    @Override
+    protected void addedToWorld(World world) {
+        getWorld().addObject(tituloAtaques, 170, 225);
+        getWorld().addObject(b1, 176, 265);
+        getWorld().addObject(b2, 176, 300);
+        getWorld().addObject(b3, 176, 335);
+        getWorld().addObject(b4, 176, 373);
+        getWorld().addObject(descripcion, 352 + 344 / 2, 300);
+    }
 
-	public void asignarCriaturaActual(Criatura criaturaActual) {
-		this.criaturaActual = criaturaActual;
+    public void asignarCriaturaActual(Criatura criaturaActual) {
+        this.criaturaActual = criaturaActual;
 
-		tituloAtaques.actualizarTexto(criaturaActual.toString());
-		descripcion.setText("");
-		botonSeleccionado = null;
-		ataque = null;
-		resetColorBotones();
-		criaturaActual.setVisualSeleccionado(true);
+        tituloAtaques.actualizarTexto(criaturaActual.toString());
+        descripcion.setText("");
+        botonSeleccionado = null;
+        ataque = null;
+        resetColorBotones();
+        criaturaActual.setVisualSeleccionado(true);
 
-		dibujarFondo();
+        dibujarFondo();
 
-		Boton[] botones = { b1, b2, b3, b4 };
+        Boton[] botones = { b1, b2, b3, b4 };
 
-		for (int i = 0; i < botones.length; i++) {
-			new BotonReactor(botones[i], this, criaturaActual, i + 1);
-		}
-	}
+        for (int i = 0; i < botones.length; i++) {
+            new BotonReactor(botones[i], this, criaturaActual, i + 1);
+        }
+    }
 
-	public void click(Criatura c) {
-		ataqueObjetivo = c;
-		if (botonSeleccionado == null) {
-			descripcion.setText(c.getStats());
-		}
-		if (ataque != null) {
-			ataque.run();
-		}
-	}
+    public void click(Criatura c) {
+        ataqueObjetivo = c;
+        if (botonSeleccionado == null) {
+            descripcion.setText(c.getStats());
+        }
+        if (ataque != null) {
+            ataque.run();
+        }
+    }
 
-	public void hover(Criatura c) {
-		if (hoverObjetivo == c) {
-			return;
-		}
+    public void hover(Criatura c) {
+        if (hoverObjetivo == c) {
+            return;
+        }
 
-		hoverObjetivo = c;
-		if (botonSeleccionado == null) {
-			// descripcion.actualizarTexto(c.toString());
-		}
-	}
+        hoverObjetivo = c;
+        if (botonSeleccionado == null) {
+            // descripcion.actualizarTexto(c.toString());
+        }
+    }
 
-	void resetColorBotones() {
-		b1.actualizar(Color.LIGHT_GRAY);
-		b2.actualizar(Color.LIGHT_GRAY);
-		b3.actualizar(Color.LIGHT_GRAY);
-		b4.actualizar(Color.LIGHT_GRAY);
-	}
+    void resetColorBotones() {
+        b1.actualizar(Color.LIGHT_GRAY);
+        b2.actualizar(Color.LIGHT_GRAY);
+        b3.actualizar(Color.LIGHT_GRAY);
+        b4.actualizar(Color.LIGHT_GRAY);
+    }
 
-	private void dibujarFondo() {
-		GreenfootImage imagenBarra = new GreenfootImage(700, 200);
-		imagenBarra.setColor(Color.BLACK);
-		imagenBarra.fill();
-		imagenBarra.setColor(criaturaActual.esEquipo1() ? Color.RED : Color.BLUE);
-		imagenBarra.fillRect(4, 4, 344, 192);
-		imagenBarra.setColor(Color.LIGHT_GRAY);
-		imagenBarra.fillRect(352, 4, 344, 192);
-		setImage(imagenBarra);
-	}
+    private void dibujarFondo() {
+        GreenfootImage imagenBarra = new GreenfootImage(700, 200);
+        imagenBarra.setColor(Color.BLACK);
+        imagenBarra.fill();
+        imagenBarra.setColor(criaturaActual.esEquipo1() ? Color.RED : Color.BLUE);
+        imagenBarra.fillRect(4, 4, 344, 192);
+        imagenBarra.setColor(Color.LIGHT_GRAY);
+        imagenBarra.fillRect(352, 4, 344, 192);
+        setImage(imagenBarra);
+    }
 
 }
--- combate-criaturas/UIInfoCriatura.java
+++ combate-criaturas/UIInfoCriatura.java
@@ -4,44 +4,50 @@ import greenfoot.GreenfootImage;
 import greenfoot.World;
 
 public class UIInfoCriatura extends Actor {
-	private static final int MARGIN = 8;
-	Criatura criatura;
-	int width = 120;
+    private static final int MARGIN = 8;
+    Criatura criatura;
+    int width = 120;
 
-	public UIInfoCriatura(Criatura criatura) {
-		this.criatura = criatura;
-	}
+    public UIInfoCriatura(Criatura criatura) {
+        this.criatura = criatura;
+    }
 
-	@Override
-	protected void addedToWorld(World world) {
-		actualizar();
-	}
+    @Override
+    protected void addedToWorld(World world) {
+        actualizar();
+    }
 
-	public void actualizar() {
-		GreenfootImage imagenNombre = new GreenfootImage(criatura.toString(), 20, Color.BLACK, null);
-		GreenfootImage imagenVida = new GreenfootImage(
-				" " + criatura.getVida() + " / " + criatura.getVidaMaxima() + " ", 20, Color.BLACK, null);
+    public void actualizar() {
+        Color colorNombre;
+        if (criatura.esEquipo1()){
+            colorNombre = Color.BLACK;
+        }else{
+            colorNombre = Color.WHITE;
+        }
+        GreenfootImage imagenNombre = new GreenfootImage(criatura.toString(), 20, colorNombre, null);
+        GreenfootImage imagenVida = new GreenfootImage(
+                " " + criatura.getVida() + " / " + criatura.getVidaMaxima() + " ", 20, Color.BLACK, null);
 
-		double barraWidth = width - MARGIN * 2.5;
-		GreenfootImage imagenBarra = new GreenfootImage(width - MARGIN * 2, 12);
-		imagenBarra.setColor(Color.RED);
+        double barraWidth = width - MARGIN * 2.5;
+        GreenfootImage imagenBarra = new GreenfootImage(width - MARGIN * 2, 12);
+        imagenBarra.setColor(Color.RED);
 
-		imagenBarra.fillRect(0, 0, (int) (barraWidth / 2), 11);
-		imagenBarra.setColor(Color.BLACK);
-		imagenBarra.drawRect(0, 0, (int) barraWidth - 1, 11);
+        imagenBarra.fillRect(0, 0, (int) (barraWidth / 2), 11);
+        imagenBarra.setColor(Color.BLACK);
+        imagenBarra.drawRect(0, 0, (int) barraWidth - 1, 11);
 
-		int height = imagenNombre.getHeight() + imagenBarra.getHeight() + imagenVida.getHeight();
+        int height = imagenNombre.getHeight() + imagenBarra.getHeight() + imagenVida.getHeight();
 
-		GreenfootImage imagen = new GreenfootImage(width, height);
+        GreenfootImage imagen = new GreenfootImage(width, height);
 
-		imagen.setColor(criatura.esEquipo1() ? Color.PINK : Color.BLUE);
-		imagen.fill();
+        imagen.setColor(criatura.esEquipo1() ? Color.PINK : Color.BLUE);
+        imagen.fill();
 
-		imagen.drawImage(imagenNombre, (width - imagenNombre.getWidth()) / 2, 0);
-		imagen.drawImage(imagenBarra, MARGIN, imagenNombre.getHeight());
-		imagen.drawImage(imagenVida, (width - imagenVida.getWidth()) / 2,
-				imagenNombre.getHeight() + imagenBarra.getHeight());
+        imagen.drawImage(imagenNombre, (width - imagenNombre.getWidth()) / 2, 0);
+        imagen.drawImage(imagenBarra, MARGIN, imagenNombre.getHeight());
+        imagen.drawImage(imagenVida, (width - imagenVida.getWidth()) / 2,
+                imagenNombre.getHeight() + imagenBarra.getHeight());
 
-		setImage(imagen);
-	}
+        setImage(imagen);
+    }
 }
