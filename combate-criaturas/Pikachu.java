0e3539d2c30bff70e4d96f1ff2d9385ef8d709e4
mathrose <mrosa@untref.edu.ar>
2023-10-01 21:11:02 -0300

    added .gitignore
    For .class files

--- combate-criaturas/Pikachu.java
+++ combate-criaturas/Pikachu.java
@@ -1,38 +1,8 @@
-﻿public class Pikachu extends Criatura {
+public class Pikachu extends Criatura {
     public Pikachu(String nombre, boolean imagenEspejada) {
         super(nombre, 21, new String[] { "Placaje", "- Vacío -", "- Vacío -", "- Vacío -" }, imagenEspejada,
                 new String[] { "Causa un daño moderado a un enemigo", "-", "-", "-" });
     }
-    // Mati: Hola!!
-
-    // Mati: Hola 2
-
-    // Diame: Hola
-    // Diame: Hola 2
-    // Mati: Hola 3
-
-    // Mati: Hola 5
-
-    // fran: hola 524
-    
-    //hols walter
-
-    // fran: hola 525
-
-    
-    // hola 
-    
-    // Hola Mati 8
-    
-
-    // Hola walter2
-    
-
-
-    // Hola Mati 9
-
-
-
 
     public Pikachu(String nombre) {
         this(nombre, false);
