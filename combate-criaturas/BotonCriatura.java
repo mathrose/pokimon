e888085c7b789a58e67022fab6d1e1ef83bd17cd
mathrose <mrosa@untref.edu.ar>
2023-11-01 12:30:36 -0300

    Pantalla seleccion cuadro de equipos

--- combate-criaturas/BotonCriatura.java
+++ combate-criaturas/BotonCriatura.java
@@ -12,18 +12,20 @@ public class BotonCriatura extends Actor {
     private int size;
     private Color color;
     private Color bgColor;
-    
+
     private BotonReactor onClick;
 
     private int forceWidth;
     private int forceHeight;
-    
+
     private GreenfootImage imagenOriginal;
-    
+
     private boolean visualHover;
     private boolean visualSeleccionado;
+    private boolean poseeEquipo = false;
+    private boolean esEquipoRojo = false;
+    private boolean esEquipoAzul = false;
 
-    
     public BotonCriatura(String nombre, GreenfootImage image) {
         this.nombre = nombre;
         this.size = size;
@@ -33,23 +35,50 @@ public class BotonCriatura extends Actor {
         render();
     }
 
+    public BotonCriatura(String nombre, GreenfootImage image, boolean esEquipoAzul) {
+        this.nombre = nombre;
+        this.size = size;
+        this.color = color;
+        this.bgColor = Color.LIGHT_GRAY;
+        this.imagenOriginal = image;
+
+        this.poseeEquipo = true;
+        if (esEquipoAzul == true) {
+            this.esEquipoAzul = true;
+            this.esEquipoRojo = false;
+        } else {
+            this.esEquipoAzul = false;
+            this.esEquipoRojo = true;
+        }
+        render();
+    }
+
     public void render() {
-            MyGreenfootImage nuevaImagen = new MyGreenfootImage(imagenOriginal) {
+        MyGreenfootImage nuevaImagen = new MyGreenfootImage(imagenOriginal) {
                 public void configurar() {
                     if (visualHover) {
                         scaleToRatio(1.15);
+                        if ((esEquipoRojo == false) && (esEquipoAzul == false)) {
+                            highlight(Color.BLACK);
+                        }
                     }
                     if (visualSeleccionado) {
                         highlight();
+                    }
+
+                    if (esEquipoAzul == true) {
+                        highlight(Color.BLUE);
+                    }
+                    if (esEquipoRojo == true) {
+                        highlight(Color.RED);
                     }
                     shadow();
                 }
             };
 
-    setImage(nuevaImagen);
+        setImage(nuevaImagen);
     }
-    
-    
+
     public void act() {
         boolean _visualHover = visualHover;
         boolean _visualSeleccionado = visualSeleccionado;
@@ -76,10 +105,10 @@ public class BotonCriatura extends Actor {
             render();
         }
     }
-    
+
     public String getNombre(){
-    
-    return this.nombre;
+
+        return this.nombre;
     }
 
 }
--- combate-criaturas/PantallaSeleccion.java
+++ combate-criaturas/PantallaSeleccion.java
@@ -19,19 +19,19 @@ public class PantallaSeleccion extends World {
     private void agregarImagenesSeleccion() {
 
         //Primera fila
-        addObject(new BotonCriatura("Blastoise", new GreenfootImage("blastoise_menu.png")), 209, 321);
-        addObject(new BotonCriatura("Charizard", new GreenfootImage("CharizardMenu.png")), 306, 321);
-        addObject(new BotonCriatura("Glaceon", new GreenfootImage("glaceon_menu.png")), 403, 321);
-        addObject(new BotonCriatura("Leafeon", new GreenfootImage("leafeon_menu.png")), 498, 321);
-        addObject(new BotonCriatura("Delphox", new GreenfootImage("delphox_menu.png")), 591, 321);
+        addObject(new BotonCriatura("Blastoise", new GreenfootImage("blastoise_menu.png")), 209, 340);
+        addObject(new BotonCriatura("Charizard", new GreenfootImage("CharizardMenu.png")), 306, 340);
+        addObject(new BotonCriatura("Glaceon", new GreenfootImage("glaceon_menu.png")), 403, 340);
+        addObject(new BotonCriatura("Leafeon", new GreenfootImage("leafeon_menu.png")), 498, 340);
+        addObject(new BotonCriatura("Delphox", new GreenfootImage("delphox_menu.png")), 591, 340);
 
         //Segunda fila
 
-        addObject(new BotonCriatura("Sylveon", new GreenfootImage("sylveon_menu.png")), 209, 416);
-        addObject(new BotonCriatura("Mew", new GreenfootImage("mew_menu.png")), 306, 416);
-        addObject(new BotonCriatura("Mimikyu", new GreenfootImage("mimikyu_menu.png")), 403, 416);
-        addObject(new BotonCriatura("Espeon", new GreenfootImage("EspeonMenu.png")), 498, 416);
-        addObject(new BotonCriatura("Charizard", new GreenfootImage("CharizardMenu.png")), 591, 416);
+        addObject(new BotonCriatura("Sylveon", new GreenfootImage("sylveon_menu.png")), 209, 430);
+        addObject(new BotonCriatura("Mew", new GreenfootImage("mew_menu.png")), 306, 430);
+        addObject(new BotonCriatura("Mimikyu", new GreenfootImage("mimikyu_menu.png")), 403, 430);
+        addObject(new BotonCriatura("Espeon", new GreenfootImage("EspeonMenu.png")), 498, 430);
+        addObject(new BotonCriatura("Charizard", new GreenfootImage("CharizardMenu.png")), 591, 430);
 
     }
 
@@ -40,33 +40,75 @@ public class PantallaSeleccion extends World {
             this.criaturasSeleccion[contadorSeleccion] = criaturaSeleccion;
             String nombreCriatura = criaturaSeleccion.getNombre();
             System.out.print(nombreCriatura);
+            int pos_x;
+            int pos_y;
+            boolean equipoAzul;
+            
+            switch (contadorSeleccion) {
+                case 0:
+                    pos_x = 65;
+                    pos_y = 88;
+                    equipoAzul = true;
+                    break;
+                case 1:
+                    pos_x = 65;
+                    pos_y = 180;
+                    equipoAzul = true;
+                    break;
+                case 2:
+                    pos_x = 728;
+                    pos_y = 88;
+                    equipoAzul = false;
+                    break;
+                case 3:
+                    pos_x = 728;
+                    pos_y = 180;
+                    equipoAzul = false;
+                    break;
+                default:
+                    pos_x = 728;
+                    pos_y = 180;
+                    equipoAzul = false;
+                    break;
+            }
+            System.out.print(pos_x);
+            
             switch (nombreCriatura) { 
                 case "Sylveon":
-                    criaturas[contadorSeleccion] = new Sylveon("Sylveon");
+                    criaturas[contadorSeleccion] = new Sylveon("Sylveon", equipoAzul);
+                    addObject(new BotonCriatura("Sylveon", new GreenfootImage("sylveon_menu.png"), equipoAzul), pos_x, pos_y);
                     break;
                 case "Blastoise":
-                    criaturas[contadorSeleccion] = new Blastoise("Blastoise");
+                    criaturas[contadorSeleccion] = new Blastoise("Blastoise",equipoAzul);
+                    addObject(new BotonCriatura("Sylveon", new GreenfootImage("sylveon_menu.png"), equipoAzul), pos_x, pos_y);
                     break;
                 case "Charizard" :
-                    criaturas[contadorSeleccion] = new Charizard("Charizard");
+                    criaturas[contadorSeleccion] = new Charizard("Charizard",equipoAzul);
+                    addObject(new BotonCriatura("Sylveon", new GreenfootImage("sylveon_menu.png"), equipoAzul), pos_x, pos_y);
                     break;
                 case "Mimikyu"   :
-                    criaturas[contadorSeleccion] = new Mimikyu("Mimikyu");
+                    criaturas[contadorSeleccion] = new Mimikyu("Mimikyu",equipoAzul);
+                    addObject(new BotonCriatura("Sylveon", new GreenfootImage("sylveon_menu.png"), equipoAzul), pos_x, pos_y);
                     break;
                 case "Mew"       :
-                    criaturas[contadorSeleccion] = new Mew("Mew");
+                    criaturas[contadorSeleccion] = new Mew("Mew",!equipoAzul);
+                    addObject(new BotonCriatura("Sylveon", new GreenfootImage("sylveon_menu.png"), equipoAzul), pos_x, pos_y);
                     break;
                 case "Delphox"   :
-                    criaturas[contadorSeleccion] = new Delphox("Delphox");
+                    criaturas[contadorSeleccion] = new Delphox("Delphox",equipoAzul);
+                    addObject(new BotonCriatura("Sylveon", new GreenfootImage("sylveon_menu.png"), equipoAzul), pos_x, pos_y);
                     break;
                 case "Espeon"    :
-                    criaturas[contadorSeleccion] = new Espeon("Espeon");
+                    criaturas[contadorSeleccion] = new Espeon("Espeon",equipoAzul);
+                    addObject(new BotonCriatura("Sylveon", new GreenfootImage("sylveon_menu.png"), equipoAzul), pos_x, pos_y);
                     break;
                 case "Glaceon"   :
-                    criaturas[contadorSeleccion] = new Glaceon("Glaceon");
+                    criaturas[contadorSeleccion] = new Glaceon("Glaceon",equipoAzul);
+                    addObject(new BotonCriatura("Sylveon", new GreenfootImage("sylveon_menu.png"), equipoAzul), pos_x, pos_y);
                     break;
                 case "Leafeon"   :
-                    criaturas[contadorSeleccion] = new Leafeon("Leafeon");
+                    criaturas[contadorSeleccion] = new Leafeon("Leafeon",equipoAzul);
+                    addObject(new BotonCriatura("Sylveon", new GreenfootImage("sylveon_menu.png"), equipoAzul), pos_x, pos_y);
                     break;
                 default:
                     break;
