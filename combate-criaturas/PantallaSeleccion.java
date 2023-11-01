b418f4efa467c77a49f8b47d42588a9d6da13fca
mathrose <mrosa@untref.edu.ar>
2023-11-01 12:39:39 -0300

    Menu dinamico aniadido

--- combate-criaturas/PantallaSeleccion.java
+++ combate-criaturas/PantallaSeleccion.java
@@ -19,17 +19,17 @@ public class PantallaSeleccion extends World {
     private void agregarImagenesSeleccion() {
 
         //Primera fila
-        addObject(new BotonCriatura("Blastoise", new GreenfootImage("blastoise_menu.png")), 209, 340);
+        addObject(new BotonCriatura("Blastoise", new GreenfootImage("BlastoiseMenu.png")), 209, 340);
         addObject(new BotonCriatura("Charizard", new GreenfootImage("CharizardMenu.png")), 306, 340);
-        addObject(new BotonCriatura("Glaceon", new GreenfootImage("glaceon_menu.png")), 403, 340);
-        addObject(new BotonCriatura("Leafeon", new GreenfootImage("leafeon_menu.png")), 498, 340);
-        addObject(new BotonCriatura("Delphox", new GreenfootImage("delphox_menu.png")), 591, 340);
+        addObject(new BotonCriatura("Glaceon", new GreenfootImage("GlaceonMenu.png")), 403, 340);
+        addObject(new BotonCriatura("Leafeon", new GreenfootImage("LeafeonMenu.png")), 498, 340);
+        addObject(new BotonCriatura("Delphox", new GreenfootImage("DelphoxMenu.png")), 591, 340);
 
         //Segunda fila
 
-        addObject(new BotonCriatura("Sylveon", new GreenfootImage("sylveon_menu.png")), 209, 430);
-        addObject(new BotonCriatura("Mew", new GreenfootImage("mew_menu.png")), 306, 430);
-        addObject(new BotonCriatura("Mimikyu", new GreenfootImage("mimikyu_menu.png")), 403, 430);
+        addObject(new BotonCriatura("Sylveon", new GreenfootImage("SylveonMenu.png")), 209, 430);
+        addObject(new BotonCriatura("Mew", new GreenfootImage("MewMenu.png")), 306, 430);
+        addObject(new BotonCriatura("Mimikyu", new GreenfootImage("MimikyuMenu.png")), 403, 430);
         addObject(new BotonCriatura("Espeon", new GreenfootImage("EspeonMenu.png")), 498, 430);
         addObject(new BotonCriatura("Charizard", new GreenfootImage("CharizardMenu.png")), 591, 430);
 
@@ -46,28 +46,28 @@ public class PantallaSeleccion extends World {
             
             switch (contadorSeleccion) {
                 case 0:
-                    pos_x = 65;
-                    pos_y = 88;
+                    pos_x = 70;
+                    pos_y = 140;
                     equipoAzul = true;
                     break;
                 case 1:
-                    pos_x = 65;
-                    pos_y = 180;
+                    pos_x = 170;
+                    pos_y = 140;
                     equipoAzul = true;
                     break;
                 case 2:
-                    pos_x = 728;
-                    pos_y = 88;
+                    pos_x = 627;
+                    pos_y = 140;
                     equipoAzul = false;
                     break;
                 case 3:
-                    pos_x = 728;
-                    pos_y = 180;
+                    pos_x = 730;
+                    pos_y = 140;
                     equipoAzul = false;
                     break;
                 default:
-                    pos_x = 728;
-                    pos_y = 180;
+                    pos_x = 150;
+                    pos_y = 140;
                     equipoAzul = false;
                     break;
             }
@@ -75,40 +75,40 @@ public class PantallaSeleccion extends World {
             
             switch (nombreCriatura) { 
                 case "Sylveon":
-                    criaturas[contadorSeleccion] = new Sylveon("Sylveon", equipoAzul);
-                    addObject(new BotonCriatura("Sylveon", new GreenfootImage("sylveon_menu.png"), equipoAzul), pos_x, pos_y);
+                    criaturas[contadorSeleccion] = new Sylveon("Sylveon", !equipoAzul);
+                    addObject(new BotonCriatura("Sylveon", new GreenfootImage("SylveonMenu.png"), equipoAzul), pos_x, pos_y);
                     break;
                 case "Blastoise":
-                    criaturas[contadorSeleccion] = new Blastoise("Blastoise",equipoAzul);
-                    addObject(new BotonCriatura("Sylveon", new GreenfootImage("sylveon_menu.png"), equipoAzul), pos_x, pos_y);
+                    criaturas[contadorSeleccion] = new Blastoise("Blastoise",!equipoAzul);
+                    addObject(new BotonCriatura("Sylveon", new GreenfootImage("BlastoiseMenu.png"), equipoAzul), pos_x, pos_y);
                     break;
                 case "Charizard" :
-                    criaturas[contadorSeleccion] = new Charizard("Charizard",equipoAzul);
-                    addObject(new BotonCriatura("Sylveon", new GreenfootImage("sylveon_menu.png"), equipoAzul), pos_x, pos_y);
+                    criaturas[contadorSeleccion] = new Charizard("Charizard",!equipoAzul);
+                    addObject(new BotonCriatura("Sylveon", new GreenfootImage("CharizardMenu.png"), equipoAzul), pos_x, pos_y);
                     break;
                 case "Mimikyu"   :
-                    criaturas[contadorSeleccion] = new Mimikyu("Mimikyu",equipoAzul);
-                    addObject(new BotonCriatura("Sylveon", new GreenfootImage("sylveon_menu.png"), equipoAzul), pos_x, pos_y);
+                    criaturas[contadorSeleccion] = new Mimikyu("Mimikyu",!equipoAzul);
+                    addObject(new BotonCriatura("Sylveon", new GreenfootImage("MimikyuMenu.png"), equipoAzul), pos_x, pos_y);
                     break;
                 case "Mew"       :
                     criaturas[contadorSeleccion] = new Mew("Mew",!equipoAzul);
-                    addObject(new BotonCriatura("Sylveon", new GreenfootImage("sylveon_menu.png"), equipoAzul), pos_x, pos_y);
+                    addObject(new BotonCriatura("Sylveon", new GreenfootImage("MewMenu.png"), equipoAzul), pos_x, pos_y);
                     break;
                 case "Delphox"   :
-                    criaturas[contadorSeleccion] = new Delphox("Delphox",equipoAzul);
-                    addObject(new BotonCriatura("Sylveon", new GreenfootImage("sylveon_menu.png"), equipoAzul), pos_x, pos_y);
+                    criaturas[contadorSeleccion] = new Delphox("Delphox",!equipoAzul);
+                    addObject(new BotonCriatura("Sylveon", new GreenfootImage("DelphoxMenu.png"), equipoAzul), pos_x, pos_y);
                     break;
                 case "Espeon"    :
-                    criaturas[contadorSeleccion] = new Espeon("Espeon",equipoAzul);
-                    addObject(new BotonCriatura("Sylveon", new GreenfootImage("sylveon_menu.png"), equipoAzul), pos_x, pos_y);
+                    criaturas[contadorSeleccion] = new Espeon("Espeon",!equipoAzul);
+                    addObject(new BotonCriatura("Sylveon", new GreenfootImage("EspeonMenu.png"), equipoAzul), pos_x, pos_y);
                     break;
                 case "Glaceon"   :
-                    criaturas[contadorSeleccion] = new Glaceon("Glaceon",equipoAzul);
-                    addObject(new BotonCriatura("Sylveon", new GreenfootImage("sylveon_menu.png"), equipoAzul), pos_x, pos_y);
+                    criaturas[contadorSeleccion] = new Glaceon("Glaceon",!equipoAzul);
+                    addObject(new BotonCriatura("Sylveon", new GreenfootImage("GlaceonMenu.png"), equipoAzul), pos_x, pos_y);
                     break;
                 case "Leafeon"   :
-                    criaturas[contadorSeleccion] = new Leafeon("Leafeon",equipoAzul);
-                    addObject(new BotonCriatura("Sylveon", new GreenfootImage("sylveon_menu.png"), equipoAzul), pos_x, pos_y);
+                    criaturas[contadorSeleccion] = new Leafeon("Leafeon",!equipoAzul);
+                    addObject(new BotonCriatura("Sylveon", new GreenfootImage("LeafeonMenu.png"), equipoAzul), pos_x, pos_y);
                     break;
                 default:
                     break;
