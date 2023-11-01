0efb1bcebacf8866832bd7753dbdcbe0a767cc73
smetana11 <walterezequielbarreiro@gmail.com>
2023-10-31 22:57:22 -0300

    Botones Avances
    estos son los botones con las clases bien escritas

--- combate-criaturas/SylveonBoton.java
+++ combate-criaturas/SylveonBoton.java
@@ -1,15 +1,15 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 
 /**
- * Write a description of class MarcoRojo here.
+ * Write a description of class SylveonBoton here.
  * 
  * @author (your name) 
  * @version (a version number or a date)
  */
-public class MarcoRojo extends BotonCreaturas
+public class SylveonBoton extends BotonCreaturas
 {
     /**
-     * Act - do whatever the MarcoRojo wants to do. This method is called whenever
+     * Act - do whatever the SylveonBoton wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
      */
     public void act()
