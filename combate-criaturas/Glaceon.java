f7a19820a8c9ab13d9ee8cc8b6968a10089d393b
SubtipoD <dylsan.martin@gmail.com>
2023-11-08 01:26:30 -0300

    Habilidades pa desarrollar

--- combate-criaturas/Glaceon.java
+++ combate-criaturas/Glaceon.java
@@ -3,7 +3,7 @@ import greenfoot.GreenfootImage;
 public class Glaceon extends Criatura {
     public Glaceon(String nombre, boolean imagenEspejada) {
         super(nombre, 21,50,40,20, "Agua", new String[] { "Placaje", "Mirada Gelida", "Nieve Pesada", "Muro Congelado" }, imagenEspejada,
-                new String[] { "Causa un daño moderado a un enemigo", "Los jugadores rivales solo pueden atacar con placaje durante 1 turno", "Realiza un ataque potente del 40% de la suma total de vida de los contrincantes", "No puede ser paralizado por 1 turno" });
+                new String[] { "Causa un daño moderado a un enemigo", "Los jugadores rivales solo pueden atacar con placaje durante 1 turno", "Realiza un ataque potente del 40% de la vida del oponente", "No puede ser paralizado por 1 turno" });
                 
 
     }
@@ -21,11 +21,14 @@ public class Glaceon extends Criatura {
     }
 
     public void atacar3(Criatura otro) {
-        atacar1(otro);
+        int porcentajeDeVida = otro.vida*40/100;
+        double efectividad = otro.recibirDaño(this, porcentajeDeVida);
+        actualizarMensajeAtaque(efectividad, 2);
+        ((PantallaDuelo)getWorld()).turno();
     }
 
     public boolean puedeRealizarAtaque3En(Criatura otro) {
-        return false;
+        return true;
     }
 
     public void atacar4(Criatura otro) {
--- combate-criaturas/Leafeon.java
+++ combate-criaturas/Leafeon.java
@@ -2,8 +2,8 @@ import greenfoot.GreenfootImage;
 
 public class Leafeon extends Criatura {
     public Leafeon(String nombre, boolean imagenEspejada) {
-        super(nombre, 21,60,50,50, "Planta", new String[] { "Placaje", "Guardian de la Hoja", "Energia del ardin", "Ciclon de Hojas" }, imagenEspejada,
-                new String[] { "Causa un daño moderado a un enemigo", "Durante 1 turno recibe solo la mitad de daño", "Regenera 20% de su vida actual o a su compañero de equipo", "Causa gran daño a ambos enemigos gastando 20% de su vida" });
+        super(nombre, 21,60,50,50, "Planta", new String[] { "Placaje", "Guardian de la Hoja", "Energia del ardin", "Sacrificio al Señor Oscuro" }, imagenEspejada,
+                new String[] { "Causa un daño moderado a un enemigo", "Durante 1 turno recibe solo la mitad de daño", "Regenera 35% de su vida actual o a su compañero de equipo", "Causa gran daño a ambos enemigos gastando 20% de su vida(creo)" });
                 
 
     }
@@ -21,7 +21,11 @@ public class Leafeon extends Criatura {
     }
 
     public void atacar3(Criatura otro) {
-        atacar1(otro);
+        int vidaRecuperada = (int)((double)this.getVidaMaxima()*0.35);
+        otro.recibirVida(otro, vidaRecuperada);
+        System.out.print(vidaRecuperada);
+        actualizarMensajeAtaque(1, 1);
+        ((PantallaDuelo)getWorld()).turno();
     }
 
     public boolean puedeRealizarAtaque3En(Criatura otro) {
@@ -29,10 +33,16 @@ public class Leafeon extends Criatura {
     }
 
     public void atacar4(Criatura otro) {
-        atacar1(otro);
+        ((PantallaDuelo)getWorld()).getObjects(Criatura.class).forEach(criatura -> {
+                criatura.recibirDaño(this, this.ataque*2);
+        });
+        int porcentajeDeVida = otro.vida*20/100;
+        this.recibirDaño(this, porcentajeDeVida);
+        actualizarMensajeAtaque(1, 1);
+        ((PantallaDuelo)getWorld()).turno();
     }
 
     public boolean puedeRealizarAtaque4En(Criatura otro) {
-        return false;
+        return true;
     }
 }
