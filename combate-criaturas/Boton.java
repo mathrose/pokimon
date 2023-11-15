14f1fd4ffd5d3de12c5e6e630fe1404dc9516f68
diamee <146673511+diamee@users.noreply.github.com>
2023-11-15 16:47:31 -0300

    Comentarios pantalla menu

--- combate-criaturas/Boton.java
+++ combate-criaturas/Boton.java
@@ -8,12 +8,12 @@ public class Boton extends Actor {
     private int size;
     private Color color;
     private Color bgColor;
-    
+
     private BotonReactor onClick;
 
     private int forceWidth;
     private int forceHeight;
-    
+
     public Boton(String texto, BotonReactor onClick, int size, Color color, int width, int height) {
         this.texto = texto;
 
@@ -38,14 +38,14 @@ public class Boton extends Actor {
             onClick.run();
         }
     }
-    
+
     public void actualizar(BotonReactor onClick) {
         this.texto = onClick.getNombreAtaque();
         this.onClick = onClick;
 
         render();
     }
-    
+
     public void actualizar(String texto, BotonReactor onClick, Color bgColor) {
         this.texto = texto;
         this.onClick = onClick;
@@ -53,29 +53,24 @@ public class Boton extends Actor {
 
         render();
     }
-    
+
     public void actualizar(Color bgColor) {
         this.bgColor = bgColor;
 
         render();
     }
-    
-    
+
     private void render() {
         // 1.: Calculate planned size based on font size and bigBorder configuration
         GreenfootImage txtImg = new GreenfootImage(" " + texto + " ", size, color, bgColor);
         int BORDER_X = 2;
         int BORDER_Y = 2;
 
-
         int height = forceHeight != 0 ? forceHeight : txtImg.getHeight() + 2 * BORDER_Y;
         int width = forceWidth != 0 ? forceWidth : txtImg.getWidth() + 2 * BORDER_X;
 
-
         // 3.: Create actual image instance
-
         GreenfootImage image = new GreenfootImage(width, height);
-
 
         // 4.: Fill background with theme main color
 
@@ -84,9 +79,7 @@ public class Boton extends Actor {
         image.setColor(bgColor);
         image.fillRect(BORDER_X, BORDER_Y, width - BORDER_X * 2, height - BORDER_Y * 2);
 
-
         // 5.: Draw text onto background
-
         image.drawImage(
             txtImg,
             (width - txtImg.getWidth()) / 2,
@@ -94,14 +87,13 @@ public class Boton extends Actor {
         );
 
 
-
-/*
+        /*
         if(drawOutline) {
-            image.setColor(elementColor("outline"));
-            for(int i=0; i<outlineWidth; i++)
-                image.drawRect(i, i, width - 2 * i - 1, height - 2 * i - 1);
+        image.setColor(elementColor("outline"));
+        for(int i=0; i<outlineWidth; i++)
+        image.drawRect(i, i, width - 2 * i - 1, height - 2 * i - 1);
         }
-*/
+         */
 
         setImage(image);
     }
--- combate-criaturas/Menu.java
+++ combate-criaturas/Menu.java
@@ -3,54 +3,39 @@ import greenfoot.*;
 public class Menu extends World {
     private Texto turnoTexto;
     private UIAtaques uiAtaques;
-    private BotonCriatura[] criaturasSeleccion = new BotonCriatura[4];
-    private BotonCriatura[] criaturasSeleccionMenu = new BotonCriatura[4];
-    private Criatura[] criaturas = new Criatura[4];
     private int contadorSeleccion = 0;
     GreenfootSound sound = new GreenfootSound("MusicaMenu.wav");
-    GreenfootSound batallasound = new GreenfootSound("batalla.wav");
-    GreenfootSound finsound = new GreenfootSound("menufin.wav");
-    GreenfootSound letsfightsound = new GreenfootSound("lets-fight.wav");
     GreenfootSound selected_sfx = new GreenfootSound("click-sfx_2.mp3");
     public int counter = 0;
 
-
     public Menu() {
+        //Dimensión en pixeles de la imagen de la pantalla
         super(800, 500, 1);
+        //Agrega la imagen menuinicio.png
         MyGreenfootImage fondoSeleccion = new MyGreenfootImage(new GreenfootImage("menuinicio.png"));
+        //Dimension en pixeles de la imagen de la pantalla
         fondoSeleccion.scale(800,500);
         getBackground().drawImage(fondoSeleccion, 0, 0);
         agregarImagenesSeleccion();
+        //Agrega el sonido de fondo MusicaMenu.wav
         this.sound=sound;
         sound.play(); 
         sound.setVolume(70);
         sound.playLoop();
-        counter++;
-
     }
-    
+
     public void pausarMusica() {
-    
+
         this.sound.pause();
-    
+
     }
 
     private void agregarImagenesSeleccion() {
 
- 
-
-        //Boton duelo
+        //Agrega el Boton Duelo a la pantalla menu
         GreenfootImage botonPlay = new GreenfootImage("BotonLuchar.png");
         botonPlay.scale(231,48);
         addObject(new BotonPlay("BotonDuelo", botonPlay),139,93);
 
     }
-    // public void finMenu() {
-         // if (counter > 1) {
-             // sound.pause();
-        
-    // }
-
-  
-// }
 }
